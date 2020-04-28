ec70c1a6feab541bfb7e141c1084423e59524bde
Toke Eskildsen <te@ekot.dk>
2020-04-28 13:33:42 +0200

    Add collection clear command

---
+++ cloud_clear.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#
+# Clears the content of a SolrCloud collection
+#
+# TODO: Figure out which cloud is running and stop it, so that version need not be specified
+#
+
+###############################################################################
+# CONFIG
+###############################################################################
+
+if [[ -s "cloud.conf" ]]; then
+    source "cloud.conf"     # Local overrides
+fi
+pushd ${BASH_SOURCE%/*} > /dev/null
+source general.conf
+: ${CLOUD:=`pwd`/cloud}
+: ${SOLR:="$HOST:$SOLR_BASE_PORT/solr"}
+: ${COLLECTION:="$1"}
+popd > /dev/null
+
+function usage() {
+    echo "Usage: ./cloud_delete.sh collection"
+    exit $1
+}
+
+check_parameters() {
+    if [ ! -d ${CLOUD}/$VERSION ]; then
+        >&2 echo "The Solr version $VERSION is not installed."
+        >&2 echo "Please run ./install_cloud.sh $VERSION"
+        usage 3
+    fi
+    if [[ "." == ".$COLLECTION" ]]; then
+        echo "No collection specified. Available collections are"
+        curl -s "$SOLR/admin/collections?action=LIST" | grep -o "<arr name=.collections.*</arr>" | grep -o "<str>[^<]*</str>" | sed 's/<\/*str>//g'
+        echo ""
+        usage 4
+    fi
+}
+
+################################################################################
+# FUNCTIONS
+################################################################################
+
+clear_collection() {
+    curl -s "$SOLR/${COLLECTION}/update?commit=true" -H 'Content-Type: text/xml' --data-binary '<delete><query>*:*</query></delete>'
+}
+
+###############################################################################
+# CODE
+###############################################################################
+
+check_parameters "$@"
+clear_collection
