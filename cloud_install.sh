d0add0738d8506d5866c52d0fd840edd4a1c7651
Toke Eskildsen <toes@kb.dk>
2022-04-20 16:44:23 +0200

    Bugfix: Send the right version to get_solr.sh

--- cloud_install.sh
+++ cloud_install.sh
@@ -43,7 +43,8 @@ check_parameters() {
 function check_package() {
     if [[ ! -s "${CACHE}/$1" ]]; then
         echo "Activating get_solr.sh as package $1 is not available at ${CACHE}/$1"
-        $SHOME/get_solr.sh "$VERSION"
+        SPECIFIC_SOLR="$VERSION"
+        . $SHOME/get_solr.sh
         if [ ! -s ${CACHE}/$1 ]; then
             >&2 "Error: Package $P not available after call to get_solr.sh"
             exit 2
